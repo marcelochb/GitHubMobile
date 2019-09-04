834eb27bc05903c971898da61e31acc315dca97e
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-09-03 22:43:37 -0300

    Acessando API do GitHub

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
-
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
 
 import { Container, Form, Input, SubmitButton } from './styles';
 
 export default class Main extends Component {
   state = {
     newUser: '',
+    users: [],
   };
 
-  handleAddUser = () => {
-    // eslint-disable-next-line react/destructuring-assignment
-    console.tron.log(this.state.newUser);
+  handleAddUser = async () => {
+    const { users, newUser } = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
   };
 
   render() {
-    const { newUser } = this.state;
+    const { users, newUser } = this.state;
     return (
       <Container>
         <Form>
